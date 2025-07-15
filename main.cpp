4a0e73596c45c790a133f5e54073f9481462d1cf
bakulman <2114964924@qq.com>
2025-07-15 16:02:19 +0800

    sync

--- main.cpp
+++ main.cpp
@@ -10,136 +10,92 @@ typedef long long ll;
 typedef unsigned long long ull;
 #define int long long 
 
-const int N = 1e5 + 100;
-int n, m, idx, root;
+const int N = 60;
+char v[N][N];
 
-struct Node{
-	int l, r, val, key, size, cnt;
-}tr[N];
+int n, m, nx, ny;
+void init() {
+	cin >> n >> m;
 
-void pushup(int p) {
-	tr[p].size = tr[tr[p].l].size + tr[tr[p].r].size + tr[p].cnt;
+	for(int i = 1; i <= n; ++i)
+		for(int j = 1; j <= m; ++j) 
+		{
+			cin >> v[i][j];
+			if(v[i][j] == '*') nx = i, ny = j;
+		}
+	return;
 }
 
-int get_node(int key) {
-	tr[++idx].key = key;
-	tr[idx].val = rand();
-	tr[idx].size = tr[idx].cnt = 1;
-	return idx;
+int tx[8]{-1, -1,0, 1, 1, 1, 0, -1};
+int ty[8]{0, 1, 1, 1, 0, -1, -1, -1};
+int vs = 0;
+int dx = 0, dy = 0;
+
+int ddx, ddy;
+bool is(int x, int y) {
+	return x >= 1 && x <= n && y >= 1 && y <= m;
 }
+void cu(int x, int y) {cout << "Crash!" << ' ' << x << ' ' << y << endl;}
+void po() {
+	if(vs == 0) {cout << nx << ' ' << ny << endl;dx = ddx, dy = ddy;}
 
-int INF = 1e8;
-void build() {
-	get_node(-INF), get_node(INF);
-	root = 1, tr[root].l = 2;
-	pushup(root);
-}
+	else if(ddx != dx && ddy != dy) {
+		f(vs) {
+			int nnx = nx + tx[ddx], nny = ny + ty[ddy];
 
-void zig(int &p) {
-	int q = tr[p].l;
-	tr[p].l = tr[q].r;
-	tr[q].r = p;
-	p = q;
-	pushup(tr[p].r), pushup(p);
-}
-
-void zag(int &p) {
-	int q = tr[p].r;
-	tr[p].r = tr[q].l;
-	tr[q].l = p;
-	p = q;
-	pushup(tr[p].l), pushup(p);
-}
-
-void inser(int x, int &p = root) {
-	if(!p) p = get_node(x);
-	else if(tr[p].key == x) tr[p].cnt++;
-	else if(tr[p].key < x) {
-		inser(tr[p].r, x);
-		if(tr[tr[p].r].val > tr[p].val)
-			zag(p);
-	}
-	else if(tr[p].key > x) {
-		inser(tr[p].l, x);
-		if(tr[tr[p].l].val > tr[p].val)
-			zig(p);
-	}
-
-	pushup(p);
-}
-
-void rm(int key, int &p = root) {
-	if(!p) return;
-
-	if(tr[p].key == key) {
-		if(tr[p].cnt > 1) tr[p].cnt--;
-		else if(tr[p].l || tr[p].r) {
-			if(!tr[p].r || tr[tr[p].l].val > tr[tr[p].r].val) {
-				zig(p);
-				rm(key, tr[p].r);
+			if(is(nnx, nny) && (v[nnx][ny] != '#' || v[nx][nny] != '#') && v[nnx][nny] != '#') {
+				nx = nnx;ny = nny;
 			}
 			else {
-				zag(p);
-				rm(key, tr[p].l);
+				cu(nx, ny);	
+				break;
 			}
-		}
-		else p = 0;
+		}		
 	}
-	else if(tr[p].key > key) rm(key, tr[p].l);
-	else rm(key, tr[p].r); 
+	else{
+		
+		f(vs) {
+			int nnx = nx + tx[ddx], nny = ny + ty[ddy];
+			if(is(nnx, nny) && v[nnx][nny] != '#') {
+				nx = nnx;ny = nny;
+			}
+			else {
+				cu(nx, ny);
+				break;
+			}
+		}	
+	}
 
-	pushup(p);
+	dx = ddx;dy = ddy;
 }
 
-int get_rank_by_key(int key, int p = root) {
-	if(!p) return 1;
+void sl(char ch) {
+	ddx = dx;ddy = dy;
+	if(ch == 'L') ddx = (dx + 8 - 1) % 8, dy = (ddy + 8 -1) % 8;
+	else if(ch == 'R') ddx = (dx + 8 + 1)  % 8, ddy = (dy + 8 + 1) % 8;
+	else  if(ch == 'U') vs++;
+	else vs = (vs-1 > 0 ? vs -1 : 0);
 
-	if(tr[p].key == key) return tr[tr[p].l].size + 1;
-	else if(tr[p].key > key) return get_rank_by_key(key, tr[p].l);
-	return tr[p].cnt + tr[tr[p].l].size + get_rank_by_key(key, tr[p].r);
-}
-
-int get_key_by_rank(int rank, int p = root) {
-	if(!rank) return INF;
-
-	if(rank <= tr[tr[p].l].size) return get_rank_by_key(rank, tr[p].l);
-	if(rank <= tr[tr[p].l].size + tr[p].cnt) return tr[p].key;
-	return get_key_by_rank(rank - tr[tr[p].l].size - tr[p].cnt, tr[p].r);
-}
-
-int get_prev(int key, int p = root) {
-	if(!p) return -INF;
-	if(tr[p].key >= key) return get_prev(tr[p].l, key);
-	return max(tr[p].key, get_prev(tr[p].r, key));
-}
-
-int get_next(int key, int p = root) {
-	if(!p) return INF;
-	if(tr[p].key <= key) return get_next(key, tr[p].r);
-	return min(tr[p].key, get_next(key, tr[p].l));
+	po();
 }
 
 signed main() {
-//	freopen("an", "r", stdin);
+	freopen("./an", "r", stdin);
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
-	build();
 
-	cin >> n;
+	init();
+	int kk;
+	cin >> kk;
 
-	for(int i = 0;i < n; ++i) {
-		int op, x;
-		cin >> op >> x;
+	string s;
+	cin >> s;
+	for(auto c : s) sl(c);
 
-		if(op == 1) inser(x);
-		else if(op == 2) rm(x);
-		else if(op == 3) cout << get_rank_by_key(x) -1 << endl;
-		else if(op == 4) cout << get_key_by_rank(x+1) << endl;
-		else if(op == 5) cout << get_prev(x) << endl;
-		else cout << get_next(x) << endl;
-	}
+
+
 
 	return 0;
 }
--- miku.cpp
+++ miku.cpp
@@ -10,50 +10,7 @@ typedef long long ll;
 typedef unsigned long long ull;
 #define int long long 
 
-constexpr int N = 1e5 + 100;
-int tree[N<<2], v[N], n, mark[N<<2];
 
-inline void push_down(int p, int len) {
-	if(len <= 1) return;   //叶节点处理
-	tree[p<<1] += mark[p] * (len - len/2);
-	tree[p<<1 | 1] += mark[p] * (len/2);
-	mark[p<<1] += mark[p];
-	mark[p<<1 | 1] +=mark[p];
-	mark[p] = 0;  //清除父节点
-}
-
-
-void build(int p = 1, int l = 1, int r = n) {
-	if(l == r) return void(tree[p] = v[l]);
-
-	int mid = (l + r) >> 1;
-
-	build(p << 1 , l, mid);
-	build(p << 1 | 1, mid + 1, r);
-	tree[p] = tree[p << 1] + tree[p << 1 | 1];
-
-
-} 
-inline int query(int l, int r, int p = 1, int cl = 1, int cr = n) {
-	if(l <= cl && r >= cr) return tree[p];
-
-	push_down(p, cr - cl + 1);
-	int mid = (cl + cr) >> 1, ans = 0;
-	if(mid >= l) ans += query(l, r, p << 1, cl, mid);
-	if(mid < r) ans += query(l, r, p << 1 | 1, mid + 1, cr);
-
-	return ans;
-}
-
-inline void update(int l, int r, int d, int p = 1, int cl = 1, int cr = n) {
-	if(l <= cl && r >= cr) {mark[p] += d, tree[p] += d * (cr - cl + 1);return;}  //更新标记，更新节点大小
-
-	push_down(p, cr - cl + 1);
-	int mid = (cl + cr) >> 1;
-	if(mid >= l) update(l, r, d, p << 1, cl, mid);
-	if(mid < r) update(l, r, d, p << 1 | 1, mid + 1, cr);
-	tree[p] = tree[p<<1] + tree[p<<1 | 1];
-}
 
 signed main() {
 //	freopen("an", "r", stdin);
@@ -61,18 +18,13 @@ signed main() {
 	cin.tie(0);
 	cout.tie(0);
 
-	int m;
-	cin >> n >> m;
-	for(int i = 1; i <= n;  ++i) cin >> v[i];
 
-	build();
-
-	while(m--) {
-		int o, l, r, d;
-		cin >> o >>l >> r;
-		if(o == 1) cin >> d, update(l, r, d);
-		else cout << query(l, r) << endl;
-	}
-
+	int n;
+	cin >> n;
+	int a, b;
+	f(n) cin >> a >> b;
+	double k = n * 1.0;
+	double d = 1.0;
+	cout <<fixed << setprecision(10) <<  d/k;
 	return 0;
 }
