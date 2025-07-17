eddf05073eddc3a3e59dc56a70e2f6da57c0686a
bakulman <2114964924@qq.com>
2025-07-17 16:53:40 +0800

    tidy directory

--- main.cpp
+++ main.cpp
@@ -10,92 +10,32 @@ typedef long long ll;
 typedef unsigned long long ull;
 #define int long long 
 
-const int N = 60;
-char v[N][N];
-
-int n, m, nx, ny;
-void init() {
-	cin >> n >> m;
-
-	for(int i = 1; i <= n; ++i)
-		for(int j = 1; j <= m; ++j) 
-		{
-			cin >> v[i][j];
-			if(v[i][j] == '*') nx = i, ny = j;
-		}
-	return;
-}
-
-int tx[8]{-1, -1,0, 1, 1, 1, 0, -1};
-int ty[8]{0, 1, 1, 1, 0, -1, -1, -1};
-int vs = 0;
-int dx = 0, dy = 0;
-
-int ddx, ddy;
-bool is(int x, int y) {
-	return x >= 1 && x <= n && y >= 1 && y <= m;
-}
-void cu(int x, int y) {cout << "Crash!" << ' ' << x << ' ' << y << endl;}
-void po() {
-	if(vs == 0) {cout << nx << ' ' << ny << endl;dx = ddx, dy = ddy;}
-
-	else if(ddx != dx && ddy != dy) {
-		f(vs) {
-			int nnx = nx + tx[ddx], nny = ny + ty[ddy];
-
-			if(is(nnx, nny) && (v[nnx][ny] != '#' || v[nx][nny] != '#') && v[nnx][nny] != '#') {
-				nx = nnx;ny = nny;
-			}
-			else {
-				cu(nx, ny);	
-				break;
-			}
-		}		
-	}
-	else{
-		
-		f(vs) {
-			int nnx = nx + tx[ddx], nny = ny + ty[ddy];
-			if(is(nnx, nny) && v[nnx][nny] != '#') {
-				nx = nnx;ny = nny;
-			}
-			else {
-				cu(nx, ny);
-				break;
-			}
-		}	
-	}
-
-	dx = ddx;dy = ddy;
-}
-
-void sl(char ch) {
-	ddx = dx;ddy = dy;
-	if(ch == 'L') ddx = (dx + 8 - 1) % 8, dy = (ddy + 8 -1) % 8;
-	else if(ch == 'R') ddx = (dx + 8 + 1)  % 8, ddy = (dy + 8 + 1) % 8;
-	else  if(ch == 'U') vs++;
-	else vs = (vs-1 > 0 ? vs -1 : 0);
-
-	po();
-}
+const int N = 5000 + 100, M = 998244353;
+int dp[N][N];
 
 signed main() {
-	freopen("./an", "r", stdin);
+//	freopen("an", "r", stdin);
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
+	int n, m;
+	cin >> n >> m;
 
-	init();
-	int kk;
-	cin >> kk;
+	dp[0][0] = 1;
 
-	string s;
-	cin >> s;
-	for(auto c : s) sl(c);
+	for(int i = 1; i <= m; ++i) dp[0][i] = 0;
 
+	for(int i = 1; i <= n; ++i) {
+		for(int j = 1; j <= m; ++j) {
+			dp[i][j] = dp[i-1][j-1] + j * dp[i-1][j];
+			dp[i][j] %= M;
+			de2(i, j)de(dp[i][j])
+		}
+	}
 
-
-
+	int ans = 1;
+	for(int i = 1; i <= m; ++i) ans = ans * i % M;
+	cout << dp[n][m] * ans % M;
 	return 0;
 }
